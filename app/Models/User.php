92c6ae473ef8d42c15eb65f3fccd8b8964d61f5a
Manoj Agrawal <manojujn@gmail.com>
2026-04-16 19:48:35 +0530

    fixed the trainer issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -75,8 +75,8 @@ class User extends Authenticatable
         return $this->hasOne(Member::class);
     }
 
-    public function trainerProfile() // <--- This name must match exactly
+    public function trainerProfile()
     {
-        return $this->hasOne(\App\Models\Trainer::class, 'user_id');
+        return $this->hasOne(\App\Models\Trainer::class, 'user_id', 'id');
     }
 }
