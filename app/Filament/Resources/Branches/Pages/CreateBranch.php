273a9ccdd113eccbfd3cf10aff4bd72bbc0e6dab
Manoj Agrawal <manojujn@gmail.com>
2026-04-17 10:16:21 +0530

    Updated user table to add status field

--- app/Filament/Resources/Branches/Pages/CreateBranch.php
+++ app/Filament/Resources/Branches/Pages/CreateBranch.php
@@ -11,7 +11,15 @@ class CreateBranch extends CreateRecord
     protected static string $resource = BranchResource::class;
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        $data['tenant_id'] = auth()->user()->getTenantId();
+        // 1. Check if 'is_main' is present and true
+        // We use data['is_main'] ?? false to prevent the "Undefined key" error
+        if (isset($data['is_main']) && $data['is_main']) {
+
+            // 2. Reset other branches for this tenant to false
+            Branch::where('tenant_id', auth()->user()->getTenantId())
+                ->where('is_main', true)
+                ->update(['is_main' => false]);
+        }
 
         return $data;
     }
