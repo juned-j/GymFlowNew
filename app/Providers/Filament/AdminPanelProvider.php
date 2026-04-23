42530cc7dbae9658730c360b288905edeba0cc6a
juned-j <junedansari9617@gmail.com>
2026-04-23 18:03:33 +0530

    diet plan

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,6 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            
             ->authGuard('web')
             ->colors([
                 'primary' => Color::Amber,
@@ -37,13 +38,14 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
-                Dashboard::class,
+                // Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
                 FilamentInfoWidget::class,
             ])
+            
             ->darkMode()
             // ->viteTheme('resources/css/filament/admin/theme.css')
             ->middleware([
