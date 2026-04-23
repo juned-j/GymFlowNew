350d74e110ad757e9003a1d0d48c65b3e97f3bf6
juned-j <junedansari9617@gmail.com>
2026-04-23 18:01:04 +0530

    diet plan

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use Filament\Support\Icons\Heroicon;
+use BackedEnum;
+
+class Dashboard extends Page
+{
+    protected string $view = 'filament.pages.dashboard';
+protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedHome;
+    protected function getHeaderWidgets(): array
+{
+    return [
+                \App\Filament\Widgets\StatsOverview::class,
+        \App\Filament\Widgets\RevenueGrowth::class,
+    //  \App\Filament\Widgets\WorkoutActivity::class,
+     \App\Filament\Widgets\MemberEngagement::class,
+
+
+    ];
+}
+}
