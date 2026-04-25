c7fe69397acf48fa9b92b6c47dec924bb0f24e9e
Manoj Agrawal <manojujn@gmail.com>
2026-04-25 11:31:54 +0530

    Country db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            CountrySeeder::class,
             RoleSeeder::class,
             TenantSeeder::class,
             MembershipPlanSeeder::class,
