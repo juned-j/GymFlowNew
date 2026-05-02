cdaa5640749680463b304cef11effdeb568b6f45
juned-j <junedansari9617@gmail.com>
2026-05-02 17:13:00 +0530

    error fix

--- app/Filament/Platform/Resources/Countries/CountryResource.php
+++ app/Filament/Platform/Resources/Countries/CountryResource.php
@@ -37,6 +37,10 @@ class CountryResource extends Resource
         ];
     }
 
+            public static function shouldRegisterNavigation(): bool
+{
+    return false;
+}
     public static function getPages(): array
     {
         return [
