228a6e5bd36b69d01d9ef6ef4a5a3bc498365284
juned-j <junedansari9617@gmail.com>
2026-05-02 17:09:57 +0530

    error fix

---
+++ app/Filament/Platform/Resources/Currencies/Schemas/CurrencyForm.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Platform\Resources\Currencies\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+class CurrencyForm
+{
+     public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+
+                TextInput::make('currency_code')
+                    ->label('Currency Code')
+                    ->required()
+                    ->maxLength(10)
+                    ->unique(ignoreRecord: true),
+
+                TextInput::make('currency_name')
+                    ->label('Currency Name')
+                    ->required()
+                    ->maxLength(255)
+                    ->unique(ignoreRecord: true),
+
+                TextInput::make('exchange_rate')
+                    ->label('Exchange Rate')
+                    ->numeric()
+                    ->required(),
+
+                TextInput::make('currency_symbol')
+                    ->label('Currency Symbol')
+                    ->maxLength(10)
+                    ->required()
+                    ->unique(ignoreRecord: true),
+
+                Toggle::make('is_default')
+                    ->label('Default Currency')
+                    ->default(false),
+
+            ]);
+    }
+}
