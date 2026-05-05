0bbbc1a919abb087a167c751850b5d7f5e7c0fa3
juned-j <junedansari9617@gmail.com>
2026-05-05 19:00:57 +0530

    error fix

--- config/mail.php
+++ config/mail.php
@@ -110,6 +110,15 @@ return [
     |
     */
 
+
+    'mailers' => [
+
+    'resend' => [
+        'transport' => 'resend',
+        'key' => env('RESEND_KEY'),
+    ],
+
+],
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel')),
