4499d07f6b0569fa32c7e15e2cfb3d7fb0e16bee
juned-j <junedansari9617@gmail.com>
2026-05-07 14:07:49 +0530

    error

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -5,9 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\Classes;
+use App\Traits\BelongsToTenant;
 
 class Booking extends Model
 {
+
+     use BelongsToTenant;
+
     protected $fillable = [
         'tenant_id',
         'user_id',
