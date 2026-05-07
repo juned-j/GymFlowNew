d9eba38d84c2cbe67194abbc269693e1b1537b7a
juned-j <junedansari9617@gmail.com>
2026-05-07 14:07:18 +0530

    error

--- app/Models/Branch.php
+++ app/Models/Branch.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\Member;
+use App\Traits\BelongsToTenant;
 
 class Branch extends Model
 {
     use HasFactory;
+     use BelongsToTenant;
 
     /**
      * The attributes that are mass assignable.
