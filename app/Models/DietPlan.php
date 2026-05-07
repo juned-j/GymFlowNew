bde5aaf4350a388c0cbd9779efbd84c2bd446b27
juned-j <junedansari9617@gmail.com>
2026-05-07 14:16:27 +0530

    error

--- app/Models/DietPlan.php
+++ app/Models/DietPlan.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use App\Traits\BelongsToTenant;
 class DietPlan extends Model
 {
     use HasFactory;
-
+use BelongsToTenant;
     protected $fillable = [
         'tenant_id',
         'trainer_id',
