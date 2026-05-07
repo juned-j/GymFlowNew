e3062b2e0ba71b327a726e52ed63bea687a79973
juned-j <junedansari9617@gmail.com>
2026-05-07 14:03:40 +0530

    error

--- app/Models/WorkoutPlan.php
+++ app/Models/WorkoutPlan.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\BelongsToTenant;
 
 class WorkoutPlan extends Model
 {
+
+use BelongsToTenant;
     protected $fillable = [
         'tenant_id',
         'name',
