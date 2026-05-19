38c1b03e0f5bc928a910b8be3e059d2753dab871
Manoj Agrawal <manojujn@gmail.com>
2026-05-19 11:04:11 +0530

    added refresh token

---
+++ database/migrations/2026_05_19_011348_add_refresh_tokens_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('refresh_token')->nullable()->after('password');
+
+            $table->timestamp('refresh_token_expiry')
+                ->nullable()
+                ->after('refresh_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn([
+                'refresh_token',
+                'refresh_token_expiry',
+            ]);
+        });
+    }
+};
